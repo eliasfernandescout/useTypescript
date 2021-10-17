4602ec35b57fc7d3b6ae54bcd097d0ad130510a0
elias fernandes <eliasfernandescout@gmail.com>
2021-10-17 00:17:16 -0300

    feat: add routes and service

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express, { response } from 'express';
 const app = express();
-import { config } from 'dotenv';
-// require('dotenv').config()
+// import { config } from 'dotenv';
+require('dotenv').config();
 
-app.get('/home', (request, response)=>{
-    response.send({
+app.get('/', (request, response)=>{
+    response.json({
         nome: 'Olá, estou aqui;'
     })
 })
 
 app.listen(process.env.PORT, ()=>{
-
+console.log(' 🟩 SERVER STARTED ON PORT 5002')
 })
